a13e1bf920c7550b167f08a961f2d90492d7393f
Joseph Wynn <joseph@wildlyinaccurate.com>
2019-10-31 22:03:29 +1300

    Don't use .env.example

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -5,9 +5,6 @@ import fs from "fs"
 if (fs.existsSync(".env")) {
   logger.debug("Using .env file to supply config environment variables")
   dotenv.config({ path: ".env" })
-} else {
-  logger.debug("Using .env.example file to supply config environment variables")
-  dotenv.config({ path: ".env.example" }) // you can delete this after you create your own .env file!
 }
 export const ENVIRONMENT = process.env.NODE_ENV
 export const SESSION_SECRET = process.env["SESSION_SECRET"]
