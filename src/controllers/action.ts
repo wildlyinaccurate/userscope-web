93d476f37c0f13039351b9efb7bcccb031a70ee6
Joseph Wynn <joseph@wildlyinaccurate.com>
2019-11-07 19:27:06 +1300

    Only define queue once

--- src/controllers/action.ts
+++ src/controllers/action.ts
@@ -3,6 +3,14 @@ import { SharedKeyCredential, StorageURL, ServiceURL, QueueURL, MessagesURL, Abo
 import { JOB_QUEUE_NAME, STORAGE_ACCOUNT_NAME, STORAGE_ACCOUNT_KEY } from "../util/config"
 import { check, validationResult } from "express-validator"
 
+const account = STORAGE_ACCOUNT_NAME
+const accountKey = STORAGE_ACCOUNT_KEY
+const sharedKeyCredential = new SharedKeyCredential(account, accountKey)
+const pipeline = StorageURL.newPipeline(sharedKeyCredential)
+const serviceURL = new ServiceURL(`https://${account}.queue.core.windows.net`, pipeline)
+const queueURL = QueueURL.fromServiceURL(serviceURL, JOB_QUEUE_NAME)
+const messagesURL = MessagesURL.fromQueueURL(queueURL)
+
 export const testAction = async (req: Request, res: Response) => {
   await check("url", "A valid URL is required")
     .isURL()
@@ -14,15 +22,6 @@ export const testAction = async (req: Request, res: Response) => {
     req.flash("errors", errors.array())
     return res.redirect("/")
   }
-
-  const account = STORAGE_ACCOUNT_NAME
-  const accountKey = STORAGE_ACCOUNT_KEY
-
-  const sharedKeyCredential = new SharedKeyCredential(account, accountKey)
-  const pipeline = StorageURL.newPipeline(sharedKeyCredential)
-  const serviceURL = new ServiceURL(`https://${account}.queue.core.windows.net`, pipeline)
-  const queueURL = QueueURL.fromServiceURL(serviceURL, JOB_QUEUE_NAME)
-  const messagesURL = MessagesURL.fromQueueURL(queueURL)
 
   try {
     const enqueueQueueResponse = await messagesURL.enqueue(Aborter.none, Buffer.from(req.body.url).toString("base64"), {
